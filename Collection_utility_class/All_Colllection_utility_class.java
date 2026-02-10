acbf02d6357dddcf7177f95ca9a8d3964b01e7b2
Tamilarasan-mac <tamilarasanrajendran@outlook.com>
2026-02-10 20:41:37 +0530

    Java All Collection Utility Class
     //Collections.sort(list);
    //Collections.reverse(list);
    //Collections.shuffle(list);
    //System.out.println(Collections.min(list));
     //System.out.println(Collections.max(list));
     //System.out.println(Collections.frequency(list,"Tamil")); value counting
    // Collections.sort(list); sort and binary search
    //int pos = Collections.binarySearch(list, "Virat"); sort and binary search

---
+++ Collection_utility_class/All_Colllection_utility_class.java
@@ -0,0 +1,27 @@
+package Collection_utility_class;
+import java.util.*;
+
+public class All_Colllection_utility_class {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        list.add("Abishek Sharma");                     //Collections.sort(list);
+        list.add("Ben Stokes");                      //Collections.reverse(list);
+        list.add("Cricket");                     //Collections.shuffle(list);
+        list.add("Dhoni");                 //System.out.println(Collections.min(list));
+        list.add("Virat");                 //System.out.println(Collections.max(list));
+        list.add("Finch");                 //System.out.println(Collections.frequency(list,"Tamil")); value counting
+                                            // Collections.sort(list); sort and binary search
+                                          //int pos = Collections.binarySearch(list, "Virat"); sort and binary searc
+
+        System.out.println(list);
+
+
+
+
+
+            }
+        };
+
+
+
+

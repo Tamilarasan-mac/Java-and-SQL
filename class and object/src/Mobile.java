6f38ffe6157152011dc90df7c7aea87e479050c6
Tamilarasan-mac <tamilarasanrajendran@outlook.com>
2026-02-16 18:27:37 +0530

    Java Class and Object

---
+++ class and object/src/Mobile.java
@@ -0,0 +1,30 @@
+public class Mobile {
+    
+    void iphone()
+
+    {
+        System.out.println("This is iphone");
+    }
+
+    void samsung()
+
+    {
+        System.out.println("This is samsung");
+    }
+
+    void vivo()
+
+    {
+        System.out.println("This is vivo");
+    }
+    
+    
+
+     public static void main(String[]args){
+
+        Mobile brandi = new Mobile();
+
+        brandi.vivo();
+     }
+    }
+    

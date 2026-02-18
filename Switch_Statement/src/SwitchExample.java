e00f54bcae795d1a83147abea68e969f2cae4cfc
Tamilarasan-mac <tamilarasanrajendran@outlook.com>
2026-02-18 14:32:48 +0530

    Switch_Statement
    SwitchExample , Example Program

---
+++ Switch_Statement/src/SwitchExample.java
@@ -0,0 +1,26 @@
+public class SwitchExample {
+    public static void main(String[] args) throws Exception {
+        int day = 8;
+
+        switch(day) {
+            case 1:
+                System.out.println("Monday");
+                break;
+
+            case 2:
+                System.out.println("Tuesday");
+                break;
+
+            case 3:
+                System.out.println("Wednesday");
+                break;
+
+            case 4:
+                System.out.println("Thursday");
+                break;
+
+            default:
+                System.out.println("Invalid Day");
+        }
+    }
+}
